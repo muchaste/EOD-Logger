0f7221f7213b03222a4c2688d30ae17cd69d0009
janscience <jan.benda@uni-tuebingen.de>
2021-07-05 23:53:32 +0200

    updated wave logger

--- eodlogger_2channel_wave/eodlogger_2channel_wave.ino
+++ eodlogger_2channel_wave/eodlogger_2channel_wave.ino
@@ -9,18 +9,18 @@
 
 // Settings: --------------------------------------------------------------------------------
 
-uint32_t samplingRate = 40000;       // samples per second and channel in Hertz
-int8_t channel0 =  A2;               // input pin for ADC0
-int8_t channel1 =  A16;              // input pin for ADC1
-int bits = 12;                       // resolution: 10bit 12bit, or 16bit
-int averaging = 8;                   // number of averages per sample: 0, 4, 8, 16, 32
+uint32_t samplingRate = 100000; // samples per second and channel in Hertz
+int8_t channel0 =  A2;          // input pin for ADC0
+int8_t channel1 =  A16;         // input pin for ADC1
+int bits = 12;                  // resolution: 10bit 12bit, or 16bit
+int averaging = 4;              // number of averages per sample: 0, 4, 8, 16, 32
 
-char path[] = "recordings";            // directory where to store files on SD card.
+char path[] = "recordings";     // directory where to store files on SD card.
 char fileName[] = "logger1-SDATETIME.wav";   // may include DATE, SDATE, TIME, STIME, DATETIME, SDATETIME, ANUM, NUM
-float fileSaveTime = 10*60;          // seconds
+float fileSaveTime = 10*60;     // seconds
 
-int stimulusFrequency = 200;         // Hertz
-int signalPins[] = {2, 3, -1};       // pins where to put out test signals
+int stimulusFrequency = 200;    // Hertz
+int signalPins[] = {2, 3, -1};  // pins where to put out test signals
 
 
 // ------------------------------------------------------------------------------------------
@@ -40,7 +40,6 @@ void setupADC() {
   aidata.setAveraging(averaging);
   aidata.setConversionSpeed(ADC_CONVERSION_SPEED::HIGH_SPEED);
   aidata.setSamplingSpeed(ADC_SAMPLING_SPEED::HIGH_SPEED);
-  aidata.setMaxFileTime(fileSaveTime);
   aidata.check();
 }
 
@@ -57,10 +56,10 @@ void openNextFile() {
   char dts[20];
   rtclock.dateTime(dts);
   file.openWave(name.c_str(), aidata, -1, dts);
-  aidata.writeData(file.file());
+  file.writeData();
   Serial.println(name);
   if (file.isOpen()) {
-    blink.set(2000, 20);
+    blink.set(5000, 5);
     blink.blink(2000, 1000);
   }
   else {
@@ -74,14 +73,15 @@ void openNextFile() {
 void setupStorage() {
   file.dataDir(path);
   file.setWriteInterval(aidata);
-  aidata.startWrite();
+  file.setMaxFileTime(fileSaveTime);
+  file.startWrite();
   openNextFile();
 }
 
 
 void storeData() {
   if (file.needToWrite()) {
-    size_t samples = aidata.writeData(file.file());
+    size_t samples = file.writeData();
     if (samples == 0) {
       blink.clear();
       Serial.println();
@@ -90,7 +90,7 @@ void storeData() {
       Serial.println("given the number of channels, averaging, sampling and conversion speed.");
       while(1) {};
     }
-    if (aidata.endWrite()) {
+    if (file.endWrite()) {
       file.close();
       blink.clear();
       openNextFile();
@@ -106,12 +106,12 @@ void setup() {
   Serial.begin(9600);
   while (!Serial && millis() < 2000) {};
   rtclock.check();
-  blink.set(5000, 20);
   setupTestSignals(signalPins, stimulusFrequency);
   setupADC();
   blink.switchOff();
   setupStorage();
   aidata.start();
+  Serial.println();
   aidata.report();
 }
 
